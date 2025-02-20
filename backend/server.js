4cd291f45899ce81650fc37725b1a254516a3372
giraen <122036189+giraen@users.noreply.github.com>
2025-02-21 02:00:18 +0800

    test for server

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,31 @@
 import 'dotenv/config'
 import express from 'express';
-import mysql from 'mysql';
+import mysql from 'mysql2';
 import cors from 'cors';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
-})
+const db = mysql.createPool({
+    uri: process.env.DATABASE_URL,
+    connectionLimit: 10,
+});
+
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error("Database connection failed:", err);
+    } else {
+        console.log("Connected to MySQL database.");
+        connection.release(); // Release connection after checking
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
