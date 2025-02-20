b977c52fa6d28d3a70f24242c7722210345abd16
giraen <122036189+giraen@users.noreply.github.com>
2025-02-21 02:39:11 +0800

    testing the fullstack

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import QrScanner from 'qr-scanner';
+import 'dotenv/config';
 import { useEffect, useRef, useState } from 'react';
 
 const App = () => {
@@ -7,7 +8,7 @@ const App = () => {
   const [scannedData, setScannedData] = useState(null);
   const [showPopup, setShowPopup] = useState(false);
   const scannerRef = useRef(null);
-  const API_URL = "https://your-backend-url.com";
+  const API_URL = process.env.API_URL;
 
   const initializeScanner = () => {
     if (scannerRef.current) {
