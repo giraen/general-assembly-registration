49a1191b3b9fe527fce3884e54effd9a81114a8f
giraen <122036189+giraen@users.noreply.github.com>
2025-02-21 08:39:17 +0800

    included the IDs with 5 id number

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,10 +20,12 @@ const App = () => {
     const scanner = new QrScanner(videoRef.current, (result) => {
       console.log("Scanned Data: ", result.data);
 
-      if (!/^TUPM-\d{2}-\d{4, 5}$/.test(result.data)) {
-        setMessage("Invalid QR Code format.");
-        setShowPopup(true);
-        return;
+      if (!/^TUPM-\d{2}-\d{4}$/.test(result.data)) {
+        if (!/^TUPM-\d{2}-\d{5}$/.test(result.data)) {
+          setMessage("Invalid QR Code format.");
+          setShowPopup(true);
+          return;
+        } 
       }
   
       setScannedData(result.data);
